6084ad4848cc062f27d92261f06c4954e753fd03
Erick Ventura <erick94111@gmail.com>
2023-02-22 23:04:49 -0600

    added auth to ms-payment service

--- payment/routes/api.php
+++ payment/routes/api.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::prefix("payment")->group(function () {
+Route::prefix("payment")->middleware(['guard.auth'])->group(function () {
 
     Route::get("resident-debts", [PaymentController::class, "getResidentReport"]);
     Route::post("restart-month", [PaymentController::class, "restartMonth"]);
